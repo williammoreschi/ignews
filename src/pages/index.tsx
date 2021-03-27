e18b663e5fa71c4795f64990e412e7601e7ae37f
William Moreschi <williammoreschi@gmail.com>
2021-03-27 17:35:13 -0300

    Pagina sendo carregada usando SSG  (Static Site Generation )

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
 import styles from '../styles/home.module.scss';
@@ -34,7 +34,7 @@ export default function Home({product}:HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IZgbDLcdfSNf9fsMlRxQ1DP', {
     expand: ['product']
   });
@@ -49,6 +49,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24 //24 hours
   }
 }
