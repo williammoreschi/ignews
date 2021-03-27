681a2def5a733cb8a67ace8ad19ecc0fff2850b7
William Moreschi <williammoreschi@gmail.com>
2021-03-27 09:21:02 -0300

    Base do projeto configurado

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import styles from '../styles/home.module.scss';
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Início | ig.news</title>
+    </Head>
+    <h1>Home Page</h1>
+    </>
+  )
+}
