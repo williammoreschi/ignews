5d1b87e1fd0420c696f0436ecfeff25ed4d4b3b3
William Moreschi <williammoreschi@gmail.com>
2021-03-30 00:07:46 -0300

    ADD: Autenticação com Next Auth (Github)

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,19 +1,27 @@
 import {FaGithub} from 'react-icons/fa';
 import {FiX} from 'react-icons/fi';
-
+import { signIn, signOut, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 export function SignInButton(){
-  const isUserLoggedIn = true;
-  return isUserLoggedIn 
+  const [session] = useSession();
+  return session 
   ? (
-    <button className={styles.signInButton} type="button">
+    <button 
+    onClick={()=> signOut()}
+    className={styles.signInButton} 
+    type="button"
+    >
     <FaGithub color="#04d361" />
-    William Moreschi
+    {session.user.name}
     <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button 
+    onClick={()=> signIn('github')}
+    className={styles.signInButton} 
+    type="button"
+    >
     <FaGithub color="#eba417" />
     Sign in with Github
     </button>
