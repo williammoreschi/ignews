3e13aadf91c7d5feab1aeee8cef62ccac62bdb6c
William Moreschi <williammoreschi@gmail.com>
2021-04-01 21:58:28 -0300

    CHG: Redirecionado para o Stripe. Convertendo o id do checkout session em uma url  para o redirecionamento do usuário

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps{
@@ -7,13 +9,19 @@ interface SubscribeButtonProps{
 
 export function SubscribeButton({priceId}:SubscribeButtonProps){
   const [session] = useSession();
-  function handleSubscribe(){
+  async function handleSubscribe(){
     if(!session){
       signIn('github');
       return;
     }
 
-    
+    try {
+      const sessionId = await api.post('/subscribe').then(response => response.data);
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout(sessionId);
+    } catch (err) {
+      console.log(err.message);
+    }
   }
   return(
     <button 
