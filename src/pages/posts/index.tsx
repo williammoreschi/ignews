dd4cd2b2e322b2c4857d10a2c5775a1209430184
William Moreschi <williammoreschi@gmail.com>
2021-04-12 21:49:55 -0300

    Configurado o Prismic CMS, e consumindo api para listar os posts na aplicação

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,67 @@
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { getPrismicClient } from '../../services/prismic';
+import styles from './styles.module.scss';
+
+interface IPost{
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface IPostsProps{
+  posts: IPost[] 
+}
+export default function Posts({ posts }: IPostsProps){
+  return (
+    <>
+      <Head>
+        <title>Post | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          { posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          )) }
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type','post')
+  ],{
+    fetch: ['post.title','post.content'],
+    pageSize : 10
+  })
+
+  const posts = response.results.map(post=>{
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleString('pt-BR',{
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  });
+  //console.log(response);
+  console.log(JSON.stringify(response,null,2))
+  return {
+    props: { posts }
+  }
+}
