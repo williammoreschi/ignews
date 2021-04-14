067a20157246096cec8c7808f90d2b106d76648b
William Moreschi <williammoreschi@gmail.com>
2021-04-14 00:03:35 -0300

    Validando assinatura pra poder visualizar o post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -39,9 +39,15 @@ export default function Post({post}:IPostProps){
 export const getServerSideProps: GetServerSideProps = async ({req, params}) => {
   const session = await getSession({req});  
   const { slug } = params;
-  //if(!session){
   
-  //}
+  if(!session || !session.activeSubscription){
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req);
 
